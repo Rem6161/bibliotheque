d1dded7ac7792d7523e47e0d5b1aae79329e3d29
Muhammad Shahid <me911khan@gmail.com>
2025-02-20 23:02:50 +0100

    finished the entities

--- src/main/java/fr/dawan/bibliotheque/entities/Borrow.java
+++ src/main/java/fr/dawan/bibliotheque/entities/Borrow.java
@@ -1,5 +1,69 @@
 package fr.dawan.bibliotheque.entities;
 
+import java.io.ObjectInputFilter.Status;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode.Exclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "borrow")
 public class Borrow extends BaseEntity {
 
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "borrow_date", nullable = false)
+	private LocalDate borrowDate;
+	
+	@Column(name = "borrow_return_date", nullable = false)
+	private LocalDate borrowReturnDate;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 15)
+	private Status status;
+	
+	@ManyToMany
+	@Exclude
+	private Set<Book> books = new HashSet<>();
+	
+	@ManyToOne
+	@Exclude
+	private Users user;
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
