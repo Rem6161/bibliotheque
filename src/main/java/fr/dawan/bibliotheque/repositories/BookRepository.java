3727ebb65cd583c26914b62202cf5c683b93e635
Muhammad Shahid <me911khan@gmail.com>
2025-02-23 23:47:36 +0100

    still working on repsitories

--- src/main/java/fr/dawan/bibliotheque/repositories/BookRepository.java
+++ src/main/java/fr/dawan/bibliotheque/repositories/BookRepository.java
@@ -3,8 +3,6 @@ package fr.dawan.bibliotheque.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import fr.dawan.bibliotheque.entities.Book;
@@ -16,5 +14,8 @@ import jakarta.transaction.Transactional;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 	 
-	List<Book> findByName(@Param("nomRechercher")String nomRechercher);
+	List<Book> findByName(String name);
+
+
+
 }
