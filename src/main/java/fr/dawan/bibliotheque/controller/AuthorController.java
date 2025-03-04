1d15e054917499e7fe6530cd7908675ef4ad38df
Muhammad Shahid <me911khan@gmail.com>
2025-03-04 11:07:54 +0100

    Author - GET method

--- src/main/java/fr/dawan/bibliotheque/controller/AuthorController.java
+++ src/main/java/fr/dawan/bibliotheque/controller/AuthorController.java
@@ -1,26 +1,39 @@
 package fr.dawan.bibliotheque.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.dawan.bibliotheque.entities.Author;
+import fr.dawan.bibliotheque.services.IAuthorService;
 import org.springframework.http.MediaType;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.HttpMediaTypeNotAcceptableException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.dawan.bibliotheque.entities.Book;
-import fr.dawan.bibliotheque.services.IBookService;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 
 @RestController
-@RequestMapping("/api/author")
+@RequestMapping("/api/biblio")
 public class AuthorController {
-	
 
+    @Autowired
+    private IAuthorService authorService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    // FInd author by id
+
+    @GetMapping(value = "/author/{id}")
+    public Author getById(@PathVariable long id) {
+        return authorService.getById(id);
+    }
 
 }
